36d11ee6fb746486fc330a6a98cb76f53d2a815b
prajwal.shinde <prajwal.shinde@ril.com>
2025-07-24 14:26:41 +0530

    card changes done

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,25 +1,61 @@
+import React from "react";
+import cloudIcon from "../assets/images/ic_cloudy.svg";
+import sunnyIcon from "../assets/images/ic_sunny.svg";
+import windyIcon from "../assets/images/ic_windy.svg";
+
 const HourlyForecast = ({ hourlyData }) => {
-  const cardData = [{ time: "Now", icon: "", tempValue: "23" }];
+  const cardData = [
+    { time: "Now", icon: cloudIcon, tempValue: "23" },
+    { time: "1 PM", icon: sunnyIcon, tempValue: "24" },
+    { time: "2 PM", icon: windyIcon, tempValue: "25" },
+    { time: "3 PM", icon: sunnyIcon, tempValue: "26" },
+  ];
 
   return (
-    <div style={{ background: "red", borderRadius: "16px", padding: "1rem" }}>
-      <h2>Hourly Forecast</h2>
-      {cardData?.map((item, index) => {
-        return (
+    <div
+      style={{
+        background: "white",
+        borderRadius: "16px",
+        padding: "1rem",
+      }}
+    >
+      <h2 style={{ color: "black", textAlign: "center", marginBottom: "1rem" }}>
+        Hourly Forecast
+      </h2>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "2rem",
+          flexWrap: "wrap",
+        }}
+      >
+        {cardData.map((item) => (
           <div
+            key={item.time}
             style={{
+              background: "lightGrey",
+              color: "black",
+              borderRadius: "12px",
+              padding: "1rem",
               display: "flex",
               flexDirection: "column",
-              columnGap: "1rem",
+              alignItems: "center",
+              width: "5rem",
+              gap: "0.5rem",
             }}
-            key={item?.time}
           >
-            <span>{item?.title}</span>
-            {/* <img src={}/> */}
-            <span>{item?.tempValue}</span>
+            <span>{item.time}</span>
+            <img
+              src={item.icon}
+              alt="weather icon"
+              style={{ width: "40px", height: "40px" }}
+            />
+            <span>{item.tempValue}°</span>
           </div>
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 };
